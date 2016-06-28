61fa6cb564a398bedfdd3d59f4bbfb3417802a56
ali <alisaleh65@yahoo.com>
2016-06-28 15:41:12 +0430

    adding cache second approach browser cache(using decorators).

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 import sys
 
 from django.conf import settings
@@ -27,7 +28,8 @@ settings.configure(
 
 from django.conf.urls import url
 from django.core.cache import cache
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import etag
 
 from django import forms
 
@@ -73,6 +75,13 @@ class ImageForm(forms.Form):
 def index(request):
     return HttpResponse("Hello World")
     
+    
+def generate_etag(request, width, height):
+    content = 'Placeholder: {0}x{1}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
+    
+    
+@etag(generate_etag)    
 def placeholder(request, width, height):
     form = ImageForm({'height':height, 'width':width})
     if form.is_valid():
