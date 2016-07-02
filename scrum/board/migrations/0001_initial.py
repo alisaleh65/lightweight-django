a69f16a8cb5714a8fa75446c9058a6aa1d771430
ali <alisaleh65@yahoo.com>
2016-07-02 18:27:49 +0430

    Simple scrum board and its database in postgresql.

---
+++ scrum/board/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Sprint',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, blank=True, default='')),
+                ('desciption', models.TextField(blank=True, default='')),
+                ('end', models.DateField(unique=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.TextField(blank=True, default='')),
+                ('status', models.SmallIntegerField(choices=[(1, 'Not Started'), (2, 'In Progress'), (3, 'Testing'), (4, 'Done')], default=1)),
+                ('order', models.SmallIntegerField(default=0)),
+                ('started', models.DateField(null=True, blank=True)),
+                ('due', models.DateField(null=True, blank=True)),
+                ('completed', models.DateField(null=True, blank=True)),
+                ('assigned', models.ForeignKey(null=True, blank=True, to=settings.AUTH_USER_MODEL)),
+                ('sprint', models.ForeignKey(null=True, blank=True, to='board.Sprint')),
+            ],
+        ),
+    ]
---
+++ scrum/scrum/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'scrum.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^api/token/', obtain_auth_token, name='api_token'),
+]
