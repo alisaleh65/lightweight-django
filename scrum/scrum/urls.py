5692e2c156940293b5224e9f21cdb1ff43cc5410
ali <alisaleh65@yahoo.com>
2016-07-02 19:55:49 +0430

    add router connections.

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import include, url
 
 from rest_framework.authtoken.views import obtain_auth_token
 
+from board.urls import router
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'scrum.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^api/token/', obtain_auth_token, name='api_token'),
+    url(r'^api/', include(router.urls)),
 ]
